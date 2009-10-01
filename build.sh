d56e028e62d126b42f5d45ef570bc92388003cfa
Hamish Friedlander <hamish@silverstripe.com>
2009-10-02 10:05:25 +1300

    Minor tweak to build script

--- build.sh
+++ build.sh
@@ -6,7 +6,9 @@ cp LICENSE /tmp/
 echo "/* jQuery.Concrete - Copyright 2009 Hamish Friedlander and SilverStripe. Version $VER. */" > /tmp/concrete.js
 
 for x in vendor/jquery.selector/jquery.class.js vendor/jquery.selector/jquery.selector.js vendor/jquery.selector/jquery.selector.specifity.js vendor/jquery.selector/jquery.selector.matches.js src/jquery.dat.js src/jquery.concrete.js ; do \
+  echo >> /tmp/concrete.js
   echo "/* $x */" >> /tmp/concrete.js
+  echo >> /tmp/concrete.js
   cat $x >> /tmp/concrete.js
   echo >> /tmp/concrete.js
 done
